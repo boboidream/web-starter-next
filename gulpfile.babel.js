5926c2edaa93f3fa035ad867ea0188174e1c28e5
v_wboahng <v_wbozhang@tencent.com>
2017-08-05 12:57:03 +0800

    init

---
+++ gulpfile.babel.js
@@ -0,0 +1,46 @@
+'use strict'
+
+import gulp from 'gulp'
+import swig from 'gulp-swig'
+import less from 'gulp-less'
+import Autoprefix from 'less-plugin-autoprefix'
+import header from 'gulp-header'
+import del from 'del'
+
+import pkg from './package.json'
+
+const opts = {
+  header: `
+/**
+* <%= pkg.name %> - <%= pkg.description %>
+* @version v<%= pkg.version %>
+* @link <%= pkg.homepage %>
+* @license <%= pkg.license %>
+*/
+`
+}
+
+gulp.task('tmpl:dev', () => {
+  return gulp.src(['./src/**/*.html', '!./src/**/_*.html'])
+      .pipe(swig())
+      .pipe(gulp.dest('./.tmp/'))
+})
+
+gulp.task('less:dev', () => {
+  return gulp.src(['./src/**/*.less', ])
+      .pipe(less({
+        plugins: [new Autoprefix()]
+      }))
+      .pipe(header(opts.header, {pkg: pkg}))
+      .pipe(gulp.dest('./.tmp/'))
+})
+
+gulp.task('clean', () => {
+  let stream = del(['.tmp/**', 'dist/**']).then((paths) => {
+    console.log(`Deleted files and folders:\n${paths.join('\n')}`)
+  })
+
+  return stream
+})
+
+gulp.task('dev', gulp.series('clean', gulp.parallel('less:dev', 'tmpl:dev')))
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Web-starter-next
+
+## Features
+gulp@4.0 
+gulpfile with es6
+
+swig -> *.html
+less -> *.css
+
+---
+## 特性
+将 `src` 下的 `.less` 文件，转为 `.css` 文件并添加浏览器前缀，忽略 `_*.less` 类型模板文件。
+
